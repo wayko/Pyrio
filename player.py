1742964c01b9bc157c1398bf2f8abe5e2ca2b45c
Jose Ortiz <wayko@users.noreply.github.com>
2023-04-18 12:06:51 -0400

    added new animation
    added new animation when user face left or right

--- player.py
+++ player.py
@@ -11,12 +11,16 @@ class Player(pygame.sprite.Sprite):
 		self.image = self.animations['idle'][self.frame_index]
 		self.rect = self.image.get_rect(topleft = pos)
 		
-
 		#player movement
 		self.direction = pygame.math.Vector2(0,0)
 		self.speed = 8
 		self.gravity = 0.8
 		self.jump_speed = -16
+
+		#player status
+		self.status = 'idle'
+		self.facing_right = True
+
 	
 	def import_character_assests(self):
 		character_path = 'assets/character/'
@@ -26,27 +30,47 @@ class Player(pygame.sprite.Sprite):
 			full_path = character_path + animation
 			self.animations[animation] = import_folder(full_path)
 			
-
 	def animate(self):
-		animation = self.animations['run']
+		animation = self.animations[self.status]
 
 		#loop over frame index
 		self.frame_index += self.animation_speed
 		if self.frame_index >= len(animation):
 			self.frame_index = 0
-		self.image = animation[int(self.frame_index)]
+		image = animation[int(self.frame_index)]
+		
+
+		if self.facing_right:
+			self.image = image
+		else:
+			flipped_image = pygame.transform.flip(image, True, False)
+			self.image = flipped_image
 
 	def get_input(self):
 		keys = pygame.key.get_pressed()
 		if keys[pygame.K_RIGHT]:
 			self.direction.x = 1
+			self.facing_right = True
 		elif keys[pygame.K_LEFT]:
 			self.direction.x = -1
+			self.facing_right = False
 		else:
 			self.direction.x = 0
 
 		if keys[pygame.K_SPACE]:
 			self.jump()
+
+	def get_status(self):
+		if self.direction.y < 0:
+			self.status = 'jump'
+		elif self.direction.y > 1:
+			self.status = 'fall'
+		else:
+			if self.direction.x != 0:
+				self.status = 'run'
+			else:
+				self.status = 'idle'
+
 
 	def apply_gravity(self):
 		self.direction.y += self.gravity
@@ -58,4 +82,5 @@ class Player(pygame.sprite.Sprite):
 	def update(self):
 		self.get_input()
 		self.import_character_assests()
+		self.get_status()
 		self.animate()
